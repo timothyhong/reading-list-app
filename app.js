033fc2fe2648553361c110a6c2f7276bc3660f5f
Tim <61671973+timothyhong@users.noreply.github.com>
2021-08-11 22:04:35 -0400

    test add of oidc auth

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { auth } = require('express-openid-connect');
+app.use(
+  auth({
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+    idpLogout: true,
+    response_type: "code",
+    scope: "openid profile bestSeller.fullaccess IdentityServerApi roles user_data"
+  })
+);
+
 require("./startup/routes")(app);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
